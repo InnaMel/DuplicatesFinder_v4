61c14ded36764d7e07ed626df1f288d368f788e3
Inna <forincha@gmail.com>
2022-01-12 22:15:52 +0200

    fixed raised exception

--- DuplicatesFinder_v4/ViewModels/DuplicatesViewModel.cs
+++ DuplicatesFinder_v4/ViewModels/DuplicatesViewModel.cs
@@ -25,7 +25,7 @@ namespace DuplicatesFinder_v4.ViewModels
             {
                 var getCurrentNameUser = Environment.UserName;
                 pathWithAppFolder = Path.Combine($"C:\\Users\\{getCurrentNameUser}\\Downloads", "DuplicatesFinder");
-                Directory.CreateDirectory(PathWithAppFolder);
+
                 return pathWithAppFolder;
             }
         }
@@ -71,7 +71,8 @@ namespace DuplicatesFinder_v4.ViewModels
         {
             var dateCreation = DateTime.Now.ToShortDateString();
             var setFileSaveName = $"Dublicates_{dateCreation}.txt";
-            var setFilePath = Path.Combine($"{PathWithAppFolder}", "{setFileSaveName}");
+            Directory.CreateDirectory(pathWithAppFolder);
+            var setFilePath = Path.Combine($"{PathWithAppFolder}", $"{setFileSaveName}");
             var option = new JsonSerializerOptions { WriteIndented = true };
 
             if (File.Exists(setFilePath))
